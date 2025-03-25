67bc8719a6f0735e2c9eed02989e0c43b400ef65
Abdalla <abdi.aly20@gmail.com>
2025-03-25 13:32:17 -0400

    added a reservation/customer form styling with Bootstrap

--- business/reservations/forms.py
+++ business/reservations/forms.py
@@ -5,19 +5,21 @@ class CustomerForm(forms.ModelForm):
     class Meta:
         model = Customer
         fields = '__all__'
-
-        def __init__(self, *args, **kwargs):
-            super(CustomerForm, self).__init__(*args, **kwargs)
-            for name, field in self.fields.items():
-                field.widget.attrs.update({'class':'input'})
+    def __init__(self, *args, **kwargs):
+        super(CustomerForm, self).__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class':'form-control'})
+        
 
 
 class ReservationForm(forms.ModelForm):
     class Meta:
         model = Reservation
         fields = ['pickup_date', 'pickup_time', 'luggage_count', 'pasenger_count', 'special_requests']
-    
     def __init__(self, *args, **kwargs):
         super(ReservationForm, self).__init__(*args, **kwargs)
         for name, field in self.fields.items():
-            field.widget.attrs.update({'class':'input'})
+            field.widget.attrs.update({'class':'form-control'})
+        self.fields['special_requests'].widget.attrs.update({'placeholder':'Any Special Requests e.g carseats, booster seats, grocery store surprise etc.',})
+    
+  
