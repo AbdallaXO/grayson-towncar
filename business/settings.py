ce6628ce0655b758db97d34789100be42607349b
Hussein Mukhtar <HushmKun@outlook.com>
2025-03-28 23:20:13 +0200

    feat: Add .gitignore and load env vars
    - Added `.gitignore` to ignore common files and directories.
    - Updated `settings.py` to load environment variables from `.env` file.
    - Added `DJANGO_SECRET_KEY` and `DJANGO_DEBUG` to `.env`.
    - `settings.py` now reads `DJANGO_SECRET_KEY` and `DJANGO_DEBUG` from the environment, providing a fallback for `DJANGO_SECRET_KEY`.

--- business/settings.py
+++ business/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-g87((!bltauc5e=93+36&kd1#c&tb7t5ew4oq0&y2=)2g)ssh9"
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', "tgvhbjbvfdr5^asuiklfugulvb#$%^YHBNKI&tgvhbjbvfdr5^")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "1"
