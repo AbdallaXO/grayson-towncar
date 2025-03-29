cf7762550bdc8faec195c863f82cc8c6d851cb4f
Abdalla <aabouelnasr@mail.valenciacollege.edu>
2025-03-29 11:39:19 -0400

    added a constants page to import choices from

--- reservations/constants.py
+++ reservations/constants.py
@@ -1,19 +1,19 @@
 CARSEAT_CHOICES = [
-    ("booster", "Booster Seat"),
-    ("rear_facing", "Rear-Facing Car Seat"),
-    ("forward_facing", "Forward-Facing Car Seat"),
-]
+        ("booster", "Booster Seat"),
+        ("rear_facing", "Rear-Facing Car Seat"),
+        ("forward_facing", "Forward-Facing Car Seat"),
+    ]
 TRIP_CHOICES = [
-    ("one_way", "One Way"),
-    ("round_trip", "Round Trip"),
-]
+        ("one_way", "One Way"),
+        ("round_trip", "Round Trip"),
+    ]
 FLIGHT_TYPE_CHOICES = [
-    ("arrival", "Arrival"),
-    ("departure", "Departure"),
-]
+        ("arrival", "Arrival"),
+        ("departure", "Departure"),
+    ]
 VEHICLE_TYPES = [
-    ("towncar", "Towncar"),
-    ("suv", "SUV"),
-    ("mini_van", "Mini Van"),
-    ("van", "Van"),
-]
+        ("towncar", "Towncar"),
+        ("suv", "SUV"),
+        ("mini_van", "Mini Van"),
+        ("van", "Van"),
+    ]
