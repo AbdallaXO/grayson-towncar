16f1182bf67cf6fec87e63ddb1ce8456c7247f94
Abdalla <aabouelnasr@mail.valenciacollege.edu>
2025-03-29 13:04:12 -0400

    Added an About page

--- reservations/urls.py
+++ reservations/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.index, name="home"),
     path("faqs/", views.faqs, name="faqs"),
     path("book/", views.BookRideView.as_view(), name="book-ride"),
+    path('about/', views.about_us, name='about-us')
 ]
