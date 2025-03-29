581654a2c8e12a7647a2a51f059dafb7d16a8080
Abdalla <aabouelnasr@mail.valenciacollege.edu>
2025-03-29 11:35:49 -0400

    created a constants file and added all choices for the models in it to import for cleaner organization

--- business/settings.py
+++ business/settings.py
@@ -23,7 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', "tgvhbjbvfdr5^asuiklfugulvb#$%^YHBNKI&tgvhbjbvfdr5^")
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY", "tgvhbjbvfdr5^asuiklfugulvb#$%^YHBNKI&tgvhbjbvfdr5^"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "1"
