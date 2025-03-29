a07b9325645b3b421869aa835ccefacd553695a9
Abdalla <aabouelnasr@mail.valenciacollege.edu>
2025-03-29 17:22:45 -0400

    added a basic skeleton form for the reservation model and a view

--- reservations/forms.py
+++ reservations/forms.py
@@ -1,2 +1,12 @@
 from django import forms
 from .models import Reservation
+
+class ReservationForm(forms.ModelForm):
+    class Meta:
+        model = Reservation
+        fields = '__all__'
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'my-custom-class'
