3ae12cd7e31d74821d2b46f38e20241e23833568
Abdalla <aabouelnasr@mail.valenciacollege.edu>
2025-03-29 21:24:18 -0400

    added a Customer Form,Reservation form, django-crispy-forms

--- reservations/forms.py
+++ reservations/forms.py
@@ -1,12 +1,15 @@
 from django import forms
-from .models import Reservation
-
+from .models import Reservation, Customer
+class CustomerForm(forms.ModelForm):
+    class Meta:
+        model = Customer
+        fields = '__all__'
 class ReservationForm(forms.ModelForm):
     class Meta:
         model = Reservation
         fields = '__all__'
     
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for visible in self.visible_fields():
-            visible.field.widget.attrs['class'] = 'my-custom-class'
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     for visible in self.visible_fields():
+    #         visible.field.widget.attrs['class'] = 'my-custom-class'
