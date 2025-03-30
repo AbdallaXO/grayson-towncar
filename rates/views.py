443743bd223142cd817aa859c20b4723f5536520
Abdalla <aabouelnasr@mail.valenciacollege.edu>
2025-03-29 22:01:28 -0400

    added some test data in rates from the models

--- rates/views.py
+++ rates/views.py
@@ -1,9 +1,17 @@
 from django.views import View
 from django.shortcuts import render, redirect, get_object_or_404
+from reservations.models import Vehicle, Rate, Route
 
 
 # Create your views here.
 def index(request):
     """a View that returns a table of Vehicles and their prices matching Oneway or Roundtrip,
     if you choose to book it will forward you to a view in reservations /book"""
+    towncar = Vehicle.objects.get(vehicle_type='towncar')
+    towncar_rate = Rate.objects.get(vehicle = towncar)
+    towncar = Vehicle.objects.get(vehicle_type='suv')
+    suv_rate = Rate.objects.get(vehicle = suv)
+
+
+    print(towncar_rate.oneway_price, suv_rate)
     return render(request, "rates/index.html")
