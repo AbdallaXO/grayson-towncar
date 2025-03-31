2b9105b7ac99cfc2d26738ce35b309403bb6ab6f
Abdalla <aabouelnasr@mail.valenciacollege.edu>
2025-03-30 20:46:31 -0400

    updated models

--- rates/views.py
+++ rates/views.py
@@ -1,17 +1,10 @@
 from django.views import View
 from django.shortcuts import render, redirect, get_object_or_404
-from reservations.models import Vehicle, Rate, Route
+from reservations.models import Vehicle, Rate, Route, Reservation
 
 
 # Create your views here.
 def index(request):
     """a View that returns a table of Vehicles and their prices matching Oneway or Roundtrip,
-    if you choose to book it will forward you to a view in reservations /book"""
-    towncar = Vehicle.objects.get(vehicle_type='towncar')
-    towncar_rate = Rate.objects.get(vehicle = towncar)
-    towncar = Vehicle.objects.get(vehicle_type='suv')
-    suv_rate = Rate.objects.get(vehicle = suv)
-
-
-    print(towncar_rate.oneway_price, suv_rate)
+    if you choose to book it will forward you to a view in reservations /book"""  
     return render(request, "rates/index.html")
