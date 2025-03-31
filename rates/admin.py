6c6341c6f9f51ad85dd9fd4de96442e1c49aa3e8
Abdalla <aabouelnasr@mail.valenciacollege.edu>
2025-03-31 14:22:31 -0400

    Enhance clarity and fix issues in rates app and reservations template - Added comments to the settings file to explain certain commands for better clarity. - Registered models in the rates app within the admin panel. - Updated the HTML template for reservations, including changes to text content. - Fixed an issue in the footer where duplicate elements were being displayed.

--- rates/admin.py
+++ rates/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Vehicle, Rate, Route
+admin.site.register(Vehicle)
+admin.site.register(Rate)
+admin.site.register(Route)
--- rates/views.py
+++ rates/views.py
@@ -1,10 +1,11 @@
 from django.views import View
 from django.shortcuts import render, redirect, get_object_or_404
-# from reservations.models import Vehicle, Rate, Route, Reservation
+from .models import Rate
 
 
 # Create your views here.
 def index(request):
     """a View that returns a table of Vehicles and their prices matching Oneway or Roundtrip,
     if you choose to book it will forward you to a view in reservations /book"""  
-    return render(request, "rates/index.html")
+    rates = Rate.objects.select_related('vehicle', 'route').all()
+    return render(request, "rates/index.html", {'rates':rates})
