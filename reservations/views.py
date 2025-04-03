f9679611fe9a426d73da77ffa70da6a952df0188
Abdalla <aabouelnasr@mail.valenciacollege.edu>
2025-04-02 23:50:17 -0400

    fixed responsivenss on phone for the rates table

--- reservations/views.py
+++ reservations/views.py
@@ -112,5 +112,6 @@ def about_us(request):
 def faqs(request):
     return render(request, "reservations/faqs.html")
 
+
 def contact(request):
     return render(request, "reservations/contact_us.html")
