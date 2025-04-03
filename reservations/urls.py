feaed876402a2320cf004e8d4429c94c85e2eb71
annadelnegro <annaluciadelnegro@gmail.com>
2025-04-02 22:36:05 -0400

    added contact us page and redesigned main pages quote section

--- reservations/urls.py
+++ reservations/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("faqs/", views.faqs, name="faqs"),
     path("about/", views.about_us, name="about-us"),
     path("reserve/<pk>", views.reservation_form, name="reserve"),
+    path("contact/", views.contact, name="contact")
 ]
--- services/views.py
+++ services/views.py
@@ -5,3 +5,4 @@ from reservations.models import *
 # Create your views here.d
 def index(request):
     return render(request, "services/index.html")
+
