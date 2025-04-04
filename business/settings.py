5e73eaefb0457cea5566ee13bc950e7c4f17682e
Hussein Mukhtar <HushmKun@outlook.com>
2025-04-04 06:43:30 +0200

    deployment quick patch

--- business/settings.py
+++ business/settings.py
@@ -175,3 +175,7 @@ EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://grayson-towncar-production.up.railway.app/',
+]
