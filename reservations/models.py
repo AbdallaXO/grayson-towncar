1323b9763e624dd015f19489f95dc597aeccd593
Abdalla <aabouelnasr@mail.valenciacollege.edu>
2025-04-04 17:57:12 -0400

    tweaked the model a little and added a stripe_payment_id to customer model

--- reservations/models.py
+++ reservations/models.py
@@ -20,6 +20,7 @@ class Customer(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    stripe_customer_id = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         """
