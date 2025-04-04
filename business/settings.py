446632142512c0178a3a4a791552b309e906d387
Abdalla <aabouelnasr@mail.valenciacollege.edu>
2025-04-03 20:01:53 -0400

    styled the email template

--- business/settings.py
+++ business/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 DEBUG = os.getenv('DJANGO_DEBUG', '0') == '1'
 EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_PORT = os.getenv('EMAIL_PORT', 587)
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
