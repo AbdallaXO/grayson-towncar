4280abe5b1d15430c9fceb2646135d7c14d54ed6
Abdalla <aabouelnasr@mail.valenciacollege.edu>
2025-04-05 02:11:25 -0400

    merged main

--- payment/urls.py
+++ payment/urls.py
@@ -13,5 +13,7 @@ urlpatterns = [
         views.save_card,
         name="save_card_checkout",
     ),
-    path("stripe/webhook/", views.stripe_webhook, name="stripe_webhook"),
+    path("stripe/webhook/", webhook.stripe_webhook, name="stripe_webhook"),
+    path("success/", views.payment_success, name="payment_success"),
+    path("failure/", views.payment_cancel, name="payment_cancel"),
 ]
