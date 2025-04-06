668aa5f973a16416dd0c18d8110d7b7e63c7e47f
Abdalla <aabouelnasr@mail.valenciacollege.edu>
2025-04-05 22:06:33 -0400

    iex requiremtnets

--- business/settings.py
+++ business/settings.py
@@ -33,7 +33,6 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "grayson-towncar-production.up.railway.app"]
 ALLOWED_HOSTS = ["grayson-towncar-production.up.railway.app", "localhost", "127.0.0.1"]
 
 
