dcf6f039ceaaeae5fb4a144d96a1cbdeb4677f3d
Abdalla <aabouelnasr@mail.valenciacollege.edu>
2025-04-06 11:39:51 -0400

    adjusted some css files structure, convereted some images to webp

--- business/settings.py
+++ business/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['graysontowncar.com', 'www.graysontowncar.com', 'localhost', '127.0.0.1', 'grayson-towncar-production.up.railway.app']
 
