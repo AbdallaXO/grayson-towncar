91c0b7e4f4d7e5255e59c3ef3c112ff367d1813c
Abdalla <aabouelnasr@mail.valenciacollege.edu>
2025-04-07 17:15:48 -0400

    Changed ?round= to 1 instead of ow and ?round=2 instead of rt just for cleaner simplicity

--- reservations/utils.py
+++ reservations/utils.py
@@ -12,10 +12,10 @@ def get_form_details(request, rate):
     """returns a trip type and returns a price based on the trip_type, if trip_type not valid
     redirects to the rates page"""
     trip_type = request.GET.get("round")
-    if trip_type == "ow":
+    if trip_type == "1":
         price = rate.oneway_price
         trip_type = "one_way"
-    elif trip_type == "rt":
+    elif trip_type == "2":
         price = rate.round_trip_price
         trip_type = "round_trip"
     else:
