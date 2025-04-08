5d340c23ea8120b9e352f15f29843a7e69164006
Abdalla <aabouelnasr@mail.valenciacollege.edu>
2025-04-08 01:52:09 -0400

    adjusted to postgres db

--- business/settings.py
+++ business/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from dotenv import load_dotenv
 from pathlib import Path
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,12 +125,15 @@ WSGI_APPLICATION = "business.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": STORAGE_PATH / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL", "sqlite:///db.sqlite3")
+    ),
+         
 }
+
 
 
 # Password validation
