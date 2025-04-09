1e820acbf25dd901647b663e9eee1803aebba6ae
Abdalla <aabouelnasr@mail.valenciacollege.edu>
2025-04-08 20:20:14 -0400

    fixed a mistake with a duplication in a function for get or create cutomer

--- payment/utils.py
+++ payment/utils.py
@@ -11,21 +11,13 @@ def get_or_create_stripe_customer(reservation):
     customer = reservation.customer
     if customer.stripe_customer_id:
         try:
-            stripe_customer = stripe.Customer.create(
+            stripe_customer = stripe.Customer.retrieve(
                 id=customer.stripe_customer_id,
             )
             return stripe_customer
         except stripe.error.InvalidRequestError:
             logger.warning("Stripe customer ID is invalid...")
-            stripe_customer = stripe.Customer.create(
-                email=customer.email,
-                name=customer.get_full_name(),
-                metadata={
-                    "reservation_id": reservation.id,
-                    "customer_db_id": customer.id,
-                    "creation_date": timezone.now().isoformat(),
-                },
-            )
+        
 
     stripe_customer = stripe.Customer.create(
         email=customer.email,
