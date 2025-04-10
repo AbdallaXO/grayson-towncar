8d56a5ca8678e83f7f8da7e87914815015578c29
annadelnegro <annaluciadelnegro@gmail.com>
2025-04-10 16:13:35 -0400

    fixed purgecss script. now it cross-checks all html and js files against all css files and saves clean version of css files into static/css/purged. also changed all link of stylesheets to now use those of /purged folder to avoid manual override. please see commment on purgecss file forinfo and also how to run the script. changed image use in services page (images not in css anymore, now importd with django)

--- purgecss-auto.js
+++ purgecss-auto.js
@@ -1,7 +1,20 @@
-/* How-To: 
-    First run: npm install -g purgecss (once for each machine)
-    From project root run: node purgecss-auto.js
-    Output will be located at static/css/purged/ with purged files */
+/* 
+  Shared Repo PurgeCSS Script
+  ---------------------------
+  First time:
+    npm install -g purgecss
+  Then from project root:
+    node purgecss-auto.js
+
+  This script purges CSS from: content/static/css/
+  and saves cleaned versions to: content/static/css/purged/
+
+  Our HTML files are now using all css under content/static/css/purged/
+    - This avoids having to manually update all css files under content/static/css/ 
+        after running purgecss script
+    - We will keep making edits to css files under content/static/css/ and run the 
+        purgecss script before pushing to deployment to ensure all css files are clean
+*/
 
 
 const fs = require("fs");
@@ -30,8 +43,8 @@ appDirs.forEach((app) => {
     }
 });
 
-// 2. Find all CSS files in static/css
-const cssDir = path.join(__dirname, "static", "css");
+// 2. Find all CSS files in content/static/css
+const cssDir = path.join(__dirname, "content", "static", "css");
 let cssFiles = [];
 if (fs.existsSync(cssDir)) {
     const files = fs.readdirSync(cssDir);
@@ -42,8 +55,26 @@ if (fs.existsSync(cssDir)) {
     });
 }
 
-// 3. Run purgecss with collected files
-const purgeCommand = `purgecss --content ${htmlFiles.join(" ")} --css ${cssFiles.join(" ")} --output ${cssDir}/purged`;
+// 3. Find all js files in content/static/js 
+const jsDir = path.join(__dirname, "content", "static", "js");
+let jsFiles = [];
+if (fs.existsSync(jsDir)) {
+    const walkDir = (dir) => {
+        const files = fs.readdirSync(dir, { withFileTypes: true });
+        files.forEach((file) => {
+            const fullPath = path.join(dir, file.name);
+            if (file.isDirectory()) {
+                walkDir(fullPath); // recurse into subdirectories
+            } else if (file.name.endsWith(".js")) {
+                jsFiles.push(fullPath.replace(/\\/g, "/"));
+            }
+        });
+    };
+    walkDir(jsDir);
+}
+
+// 4. Run purgecss with collected files
+const purgeCommand = `purgecss --content ${[...htmlFiles, ...jsFiles].join(" ")} --css ${cssFiles.join(" ")} --output ${cssDir}/purged`;
 
 console.log("🧼 Running PurgeCSS...\n");
 exec(purgeCommand, (err, stdout, stderr) => {
