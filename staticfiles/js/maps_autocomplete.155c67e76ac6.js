69ec57ba6dc1c98b95a85de6be405e65f7858bbc
Abdalla <aabouelnasr@mail.valenciacollege.edu>
2025-04-11 18:03:53 -0400

    fixing the build

---
+++ staticfiles/js/maps_autocomplete.155c67e76ac6.js
@@ -0,0 +1,53 @@
+function preventEnterExceptOnSpecialRequests(formSelector = "form") {
+    const form = document.querySelector(formSelector);
+    if (!form) return;
+
+    form.addEventListener("keydown", function (e) {
+        if (e.key === "Enter") {
+            const target = e.target;
+            // Check both id and name attributes to be safe
+            const isAllowedField =
+                target.id === "special_Requests" ||
+                target.name === "special_requests";
+
+            if (!isAllowedField) {
+                e.preventDefault();
+            }
+        }
+    });
+}
+
+function initAutocomplete() {
+    const legCount = 2; // since we only support round_trips
+
+    for (let i = 1; i <= legCount; i++) {
+        const pickupFieldList = document.getElementsByName(`leg${i}-pickup_location`);
+        const dropoffFieldList = document.getElementsByName(`leg${i}-dropoff_location`);
+
+        const pickupField = pickupFieldList[0];
+        const dropoffField = dropoffFieldList[0];
+
+        if (pickupField) {
+            new google.maps.places.Autocomplete(pickupField, {
+                componentRestrictions: { country: ["us"] },
+                fields: ["address_components", "geometry"],
+                types: ["establishment"]
+            });
+        }
+
+        if (dropoffField) {
+            new google.maps.places.Autocomplete(dropoffField, {
+                componentRestrictions: { country: ["us"] },
+                fields: ["address_components", "geometry"],
+                types: ["establishment"]
+            });
+        }
+    }
+}
+
+window.addEventListener("load", function () {
+    if (window.google) {
+        initAutocomplete();
+    }
+    preventEnterExceptOnSpecialRequests("#reservation_form");
+});
