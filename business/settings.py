6f3bbccb791183df57001053d6c8254e9f0bc5da
Abdalla <aabouelnasr@mail.valenciacollege.edu>
2025-04-23 15:28:48 -0400

    added jazzmin for the admin dashboard

--- business/settings.py
+++ business/settings.py
@@ -51,6 +51,7 @@ ALLOWED_HOSTS = [
 # Application definition
 # Native and third-party apps
 NATIVE_APPS = [
+    "jazzmin",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
